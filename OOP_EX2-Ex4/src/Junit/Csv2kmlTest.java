06679d15e401fffbff9f843eb32420b94a5c2b51
Eli <44745133+Justrygh@users.noreply.github.com>
2018-12-01 16:50:00 +0200

    Update Csv2kmlTest.java

--- OOP_EX2-Ex4/src/Junit/Csv2kmlTest.java
+++ OOP_EX2-Ex4/src/Junit/Csv2kmlTest.java
@@ -13,8 +13,8 @@ class Csv2kmlTest {
 	void testWrite() {
 		File M1 = new File("/Users/qusaitrabeh/Downloads/GPS-master/data/MultiCSV.kml");
 		File M2 = new File("/Users/qusaitrabeh/Downloads/GPS-master/data/MultiCSV.kml");
-		Csv2kml c1 = new Csv2kml("/Users/qusaitrabeh/Downloads/GPS-master/data/MultiCSV.kml");
-		Csv2kml c2 = new Csv2kml("/Users/qusaitrabeh/Downloads/GPS-master/data/MultiCSV.kml");
+		Csv2kml c1 = new Csv2kml("Your/Directory/Path/OOP_EX2-EX4-master/data/MultiCSV.kml");
+		Csv2kml c2 = new Csv2kml("Your/Directory/Path/OOP_EX2-EX4-master/data/MultiCSV.kml");
 		c1.write();
 		c2.write();
 		assertEquals(M1.canWrite(),M2.canWrite());
@@ -22,8 +22,8 @@ class Csv2kmlTest {
 
 	@Test
 	void testRead() {
-		Csv2kml c1 = new Csv2kml("/Users/qusaitrabeh/Downloads/GPS-master/data/MultiCSV.kml");
-		Csv2kml c2 = new Csv2kml("/Users/qusaitrabeh/Downloads/GPS-master/data/MultiCSV.kml");
+		Csv2kml c1 = new Csv2kml("Your/Directory/Path/OOP_EX2-EX4-master/data/MultiCSV.kml");
+		Csv2kml c2 = new Csv2kml("Your/Directory/Path/OOP_EX2-EX4-master/data/MultiCSV.kml");
 		ourFile file1 = c1.read();
 		ourFile file2 = c2.read();
 	assertTrue(file1.getName().equals(file2.getName()));
